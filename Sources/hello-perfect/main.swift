624500358dde0074632ebc2783fed08f0492f06c
Tri Ngo <ngominhtrint@gmail.com>
2017-10-25 12:07:01 +0700

    Finished POST request to add more people

--- Sources/hello-perfect/main.swift
+++ Sources/hello-perfect/main.swift
@@ -18,8 +18,7 @@ routes.add(method: .get, uri: "/", handler: {
     response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, Perfect!</body></html>")
     // Signalling that the request is completed
     response.completed()
-}
-)
+})
 
 
 // Adding a route to handle the GET people list URL
@@ -34,13 +33,40 @@ routes.add(method: .get, uri: "/api/v1/people", handler: {
     response.appendBody(string: people.list())
     // Signalling that the request is completed
     response.completed()
+})
+
+// Adding the route to handle the POST people add URL, with post body params
+routes.add(method: .post, uri: "/api/v1/people") { request, response in
+    let people = People()
+    
+    // Setting the response content type explicitly to application/json
+    response.setHeader(.contentType, value: "application/json")
+    // Adding a new person, passing the complete HTTPRequest object to the function
+    response.appendBody(string: people.add(request))
+    // Signalling that the request is completed
+    response.completed()
 }
-)
+
+routes.add(method: .post, uri: "/api/v1/people/json") { request, response in
+    let people = People()
+    
+    // Setting the response content type explicitly to application/json
+    response.setHeader(.contentType, value: "application/json")
+    // Adding a new person, passing the just the request's post body as a raw string to the function
+    response.appendBody(string: people.add(request.postBodyString!))
+    // Signalling that the request is completed
+    response.completed()
+}
+
+// Add the routes to the server
+server.addRoutes(routes)
+
+// Set a listen port of 8181
+server.serverPort = 8181
 
 do {
     // Launch the HTTP server.
-    try HTTPServer.launch(
-        .server(name: "www.example.ca", port: 8181, routes: routes))
+    try server.start()
 } catch {
     fatalError("\(error)") // fatal error launching one of the servers
 }
